1b8d4bdb764b347556ae7d9c78d59357032360e8
Kui-Feng Lee <thinker.li@gmail.com>
2023-04-18 17:36:51 -0700

    bpftool: Show map IDs along with struct_ops links.
    A new link type, BPF_LINK_TYPE_STRUCT_OPS, was added to attach
    struct_ops to links. (226bc6ae6405) It would be helpful for users to
    know which map is associated with the link.
    
    The assumption was that every link is associated with a BPF program, but
    this does not hold true for struct_ops. It would be better to display
    map_id instead of prog_id for struct_ops links. However, some tools may
    rely on the old assumption and need a prog_id.  The discussion on the
    mailing list suggests that tools should parse JSON format. We will maintain
    the existing JSON format by adding a map_id without removing prog_id. As
    for plain text format, we will remove prog_id from the header line and add
    a map_id for struct_ops links.
    
    Signed-off-by: Kui-Feng Lee <kuifeng@meta.com>
    Reviewed-by: Quentin Monnet <quentin@isovalent.com>

--- tools/bpf/bpftool/link.c
+++ tools/bpf/bpftool/link.c
@@ -195,6 +195,10 @@ static int show_link_close_json(int fd, struct bpf_link_info *info)
 				 info->netns.netns_ino);
 		show_link_attach_type_json(info->netns.attach_type, json_wtr);
 		break;
+	case BPF_LINK_TYPE_STRUCT_OPS:
+		jsonw_uint_field(json_wtr, "map_id",
+				 info->struct_ops.map_id);
+		break;
 	default:
 		break;
 	}
@@ -227,7 +231,10 @@ static void show_link_header_plain(struct bpf_link_info *info)
 	else
 		printf("type %u  ", info->type);
 
-	printf("prog %u  ", info->prog_id);
+	if (info->type == BPF_LINK_TYPE_STRUCT_OPS)
+		printf("map_id %u  ", info->struct_ops.map_id);
+	else
+		printf("prog %u  ", info->prog_id);
 }
 
 static void show_link_attach_type_plain(__u32 attach_type)
