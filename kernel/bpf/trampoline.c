90805b3cdda7bc1cae0b32414278518c31673205
Yafang Shao <laoar.shao@gmail.com>
2023-05-09 15:15:11 +0000

    bpf: Show total linked progs cnt instead of selector in trampoline ksym
    After commit e21aa341785c ("bpf: Fix fexit trampoline."), the selector
    is only used to indicate how many times the bpf trampoline image are
    updated and been displayed in the trampoline ksym name. After the
    trampoline is freed, the count will start from 0 again.
    So the count is a useless value to the user, we'd better
    show a more meaningful value like how many progs are linked to this
    trampoline. After that change, the selector can be removed eventally.
    If the user want to check whether the bpf trampoline image has been updated
    or not, the user can also compare the address. Each time the trampoline
    image is updated, the address will change consequently.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>

--- kernel/bpf/trampoline.c
+++ kernel/bpf/trampoline.c
@@ -410,11 +410,10 @@ static int bpf_trampoline_update(struct bpf_trampoline *tr, bool lock_direct_mut
 		err = unregister_fentry(tr, tr->cur_image->image);
 		bpf_tramp_image_put(tr->cur_image);
 		tr->cur_image = NULL;
-		tr->selector = 0;
 		goto out;
 	}
 
-	im = bpf_tramp_image_alloc(tr->key, tr->selector);
+	im = bpf_tramp_image_alloc(tr->key, total);
 	if (IS_ERR(im)) {
 		err = PTR_ERR(im);
 		goto out;
@@ -451,8 +450,7 @@ again:
 
 	set_memory_rox((long)im->image, 1);
 
-	WARN_ON(tr->cur_image && tr->selector == 0);
-	WARN_ON(!tr->cur_image && tr->selector);
+	WARN_ON(tr->cur_image && total == 0);
 	if (tr->cur_image)
 		/* progs already running at this address */
 		err = modify_fentry(tr, tr->cur_image->image, im->image, lock_direct_mutex);
@@ -482,7 +480,6 @@ again:
 	if (tr->cur_image)
 		bpf_tramp_image_put(tr->cur_image);
 	tr->cur_image = im;
-	tr->selector++;
 out:
 	/* If any error happens, restore previous flags */
 	if (err)
